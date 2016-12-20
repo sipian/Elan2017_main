47ea226601fdf6980c15dc3b81781d816b0455e9
Prasanth Mamakar Komaragiri <es14btech11010@iith.ac.in>
2016-12-20 12:04:24 +0530

    team buttons left

--- team/Team_Unstopables_edgeActions.js
+++ team/Team_Unstopables_edgeActions.js
@@ -45,7 +45,85 @@ Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",8920,function(sym,
 //Edge binding end
 Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",9436,function(sym,e){});
 //Edge binding end
+Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",1068,function(sym,e){sym.stop();});
+//Edge binding end
+Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",1131,function(sym,e){sym.stop();});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_Ellipse2}","mouseout",function(sym,e){});
+//Edge binding end
 Symbol.bindElementAction(compId,symbolName,"${_hospiCore}","mouseover",function(sym,e){});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_surendraFB}","click",function(sym,e){window.open("https://www.facebook.com/surender.chauhan.7399?fref=nf","_self");});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_rajeshFB}","click",function(sym,e){window.open("https://www.facebook.com/rajesh.varagani.3","_self");});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_BSaiHarshaFB}","click",function(sym,e){window.open("https://www.facebook.com/sai.harsha.14661?fref=grp_mmbr_list","_self");});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_atulFb}","click",function(sym,e){window.open("https://www.facebook.com/atulraj297?ref=br_rs","_self");});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_ishanFb}","click",function(sym,e){window.open("https://www.facebook.com/pluto1515?fref=ts","_self");});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_vasudhaFb}","click",function(sym,e){window.open("https://www.facebook.com/vasudha.kapre?fref=ts","_self");});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_siddarthFb}","click",function(sym,e){window.open("https://www.facebook.com/siddharth.sonkamble.161?fref=ts","_self");});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_abhinauFB}","click",function(sym,e){window.open("https://www.facebook.com/nownow.21?fref=ts","_self");});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_saiSumanFB}","click",function(sym,e){window.open("https://www.facebook.com/profile.php?id=100007366119357&fref=ts","_self");});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_lalithSaiFb}","click",function(sym,e){window.open("https://www.facebook.com/lalith.sai.srinivas?fref=ts","_self");});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_pitrodaFB}","click",function(sym,e){window.open("https://www.facebook.com/yash.pitroda1?fref=ts","_self");});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_anandFB}","click",function(sym,e){window.open("https://www.facebook.com/anand.swain.97?fref=ts","_self");});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_nandaFb}","click",function(sym,e){window.open("https://www.facebook.com/nandu007d?fref=ts","_self");});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_jaladiFB}","click",function(sym,e){window.open("https://www.facebook.com/name.is.harsha?fref=ts","_self");});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_karanJindalFB}","click",function(sym,e){window.open("https://www.facebook.com/karan.jindal.146?fref=ts","_self");});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_ambujFB}","click",function(sym,e){window.open("https://www.facebook.com/ambuj05?fref=ts","_self");});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_suprabathFB}","click",function(sym,e){window.open("https://www.facebook.com/suprabathreddy9","_self");});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_viabhavFB}","click",function(sym,e){window.open("https://www.facebook.com/vaibhav.singh.330?fref=nf","_self");});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_medhaFB}","click",function(sym,e){window.open("https://www.facebook.com/profile.php?id=100008183086108&fref=ts","_self");});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_anirudhFb}","click",function(sym,e){window.open("https://www.facebook.com/anirudh.chitturi.3?fref=ts","_self");});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_manishFB}","click",function(sym,e){window.open("https://www.facebook.com/iamymr001?fref=ts","_self");});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_prathimaFb}","click",function(sym,e){window.open("https://www.facebook.com/profile.php?id=100011253270969&fref=ts","_self");});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_asutoshFB}","click",function(sym,e){window.open("https://www.facebook.com/The.Ashutosh.Sharma?fref=ts","_self");});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_abhishekFB}","click",function(sym,e){window.open("https://www.facebook.com/profile.php?id=100001050801477&fref=ts","_self");});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_sukrutFB}","click",function(sym,e){window.open("https://www.facebook.com/sukrut.rao?fref=ts","_self");});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_srujanFB}","click",function(sym,e){window.open("https://www.facebook.com/srujanbazar?fref=ts","_self");});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_abhiGuptaFB}","click",function(sym,e){window.open("https://www.facebook.com/abhinav.gupta1697?fref=ts","_self");});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_ragavFB}","click",function(sym,e){window.open("https://www.facebook.com/Raaghav.ravi97","_self");});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_pankajFb}","click",function(sym,e){window.open("https://www.facebook.com/chait.bhagwat?fref=ts","_self");});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_gouravChobeFB}","click",function(sym,e){window.open("https://www.facebook.com/gaurav.chobe21?fref=ts","_self");});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_shivomFB}","click",function(sym,e){window.open("https://www.facebook.com/profile.php?id=100012115215925&fref=ts","_self");});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_chirayuFB}","click",function(sym,e){window.open("https://www.facebook.com/chirayu.kothari.1?fref=ts","_self");});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_chiathuFB}","click",function(sym,e){window.open("https://www.facebook.com/chait.bhagwat?fref=ts","_self");});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_harshFB}","click",function(sym,e){window.open("https://www.facebook.com/harshaga97","_self");});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_prasanthFB}","click",function(sym,e){window.open("https://www.facebook.com/prasanth.mamakar?fref=ts","_self");});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_sruthiFB}","click",function(sym,e){window.open("https://www.facebook.com/shruti.bhatambre?fref=ts","_self");});
 //Edge binding end
 })("stage");
 //Edge symbol end:'stage'
